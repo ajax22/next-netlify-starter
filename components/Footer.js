cfe99ea25a135c5017fa2d1b9c1129cfd65c8194
aanarkar <ajinkya.narkar@skillsoft.com>
2021-03-18 17:10:18 +0530

    Fix: minor changes to get more data

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,11 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <div>
+        <a href="https://twitter.com/ajinkyanarkar22" rel="me">Twitter</a>
+        <a href="https://www.instagram.com/ajax221990/" rel="me">Instagram</a>
+        <a href="https://www.facebook.com/ajinkya.narkar1/" rel="mew">Facebook</a>
+        </div>
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,57 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [tweets, setTweets] = useState([])
+  const [count, setCounts] = useState({})
+  useEffect(() => {
+    fetch("https://webmention.io/api/mentions.jf2?domain=testwebmention.netlify.app&token=cDfaWLs6IaSZcg9zWUq2dQ")
+    .then(response => response.json())
+    .then(responseJson => setTweets(responseJson.children))
+
+    fetch("https://webmention.io/api/count.json?target=https://testwebmention.netlify.app/")
+    .then(response => response.json())
+    .then(responseJson => setCounts({
+      likes: responseJson.type.like || 0 + responseJson.type.repost || 0,
+      mentions: responseJson.type.mention || 0 + responseJson.type.reply || 0,
+      total: responseJson.count || 0,
+    }))
+  })
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Web Mention Demo</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="webmention" href="https://webmention.io/testwebmention.netlify.app/webmention" />
         <link rel="pingback" href="https://webmention.io/testwebmention.netlify.app/xmlrpc" />
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <a href="https://twitter.com/ajinkyanarkar22" rel="me">Twitter</a>
-        <a href="https://www.instagram.com/ajax221990/" rel="me">Instagram</a>
+        <Header title="Webmention App" />
+        <h4>Count of:</h4>
+        <div>
+          <span class="count">Total: {count.total}</span>
+          <span class="count">Mentions: {count.mentions}</span>
+          <span class="count">Likes: {count.likes}</span>
+        </div>
+        <div class="w3-container">
+  <h2>Consolidated Webmentions:</h2>
+  <ul class="w3-ul w3-card-4">
+          {tweets.map((tweet) => (
+    <li class="w3-bar">
+    <img src={tweet.author.photo} class="bg w3-bar-item w3-circle w3-hide-small" style={{width: '85px'}} />
+      <div class="w3-bar-item">
+        <span class="w3-large">{tweet.author.name}</span>
+        <div dangerouslySetInnerHTML= {{__html: tweet.content.html}}></div>
+      </div>
+    </li>
+            
+          ))}
+           </ul>
+    </div>
       </main>
 
       <Footer />
