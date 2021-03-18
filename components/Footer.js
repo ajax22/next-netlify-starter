8ff00791de58d5ff3104c8eafc865e0d39d4cf82
aanarkar <ajinkya.narkar@skillsoft.com>
2021-03-18 17:20:56 +0530

    FIX: adding github profile

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,13 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-        <div>
-        <a href="https://twitter.com/ajinkyanarkar22" rel="me">Twitter</a>
-        <a href="https://www.instagram.com/ajax221990/" rel="me">Instagram</a>
-        <a href="https://www.facebook.com/ajinkya.narkar1/" rel="mew">Facebook</a>
+        <div style={{display: 'block'}}>
+          <ul style={{display: 'flex', listStyle: 'none'}}>
+          <li style={{padding: '10px'}}><a href="https://twitter.com/ajinkyanarkar22" rel="me">Twitter</a></li>
+          <li style={{padding: '10px'}}><a href="https://www.instagram.com/ajax221990/" rel="me">Instagram</a></li>
+          <li style={{padding: '10px'}}><a href="https://www.facebook.com/ajinkya.narkar1/" rel="mew">Facebook</a></li>
+          <li style={{padding: '10px'}}><a href="https://github.com/ajax22" rel="me">Github</a></li>
+          </ul>
         </div>
       </footer>
     </>
